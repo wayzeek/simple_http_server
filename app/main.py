2d8a43714e185c58102bcf4e94000f29628ad13e
vcart <112975047+wayzeek@users.noreply.github.com>
2024-08-23 14:30:36 -0400

    add: plugged the router to API "

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
+from app.routes import router
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to my FastAPI project!"}
+app.include_router(router)
